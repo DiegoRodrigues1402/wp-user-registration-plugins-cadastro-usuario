ced5bd1478eb46e830f9cfaace34a5ab5906a08e
DiegoRodrigues1402 <diego.rodrigues1402@gmail.com>
2023-11-28 12:52:21 -0300

    atualização 2
    atualização

---
+++ update-usuario/wp-plugin-update-usuario/wp-plugin-update-usuario.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+Plugin Name:wp-plugin-update-Usuario
+Description: Update usuario para login
+Version: 1.0
+Author: Back end - Diego Rodrigues
+Author URI:linkedin.com/in/diego-flores-rodrigues-0924ab42/
+License: GPL2
+*/
+include __DIR__ . '/includes/user/updateUsuario.php';
+Update::init();
+?>
